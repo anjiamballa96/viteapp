a1d9129c5b07c0ec06aa5b4d013790b2c78d1da2
anjiamballa <anjiamballa96@gmail.com>
2024-03-11 18:17:04 +0530

    Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import { GrProjects } from "react-icons/gr";
+import { GiSkills } from "react-icons/gi";
+import { LuNetwork } from "react-icons/lu";
 
 function Home() {
   return (
-    <div>Homeeeeeeeeeeeeeeeeee</div>
-  )
+    <div>
+      <div class="intro">
+        <h1>React Web Developer</h1>
+        <p className="w-3/4 text-balance">
+          React is a JavaScript library for building user interfaces.React has
+          been designed from the start for gradual adoption, and you can use as
+          little or as much React as you need. Whether you want to get a taste
+          of React, add some interactivity to a simple HTML page, or start a
+          complex React-powered app, the links in this section will help you get
+          started.
+        </p>
+        <button className="text-white bg-blue-500 mt-4">Learn More</button>
+      </div>
+      <div class="achievements">
+        <div class="work">
+          <i class=""><GrProjects /></i>
+          <p class="work-heading">Projects</p>
+          <p class="work-text">
+            I have worked on many projects and I am very proud of them. I am a
+            very good developer and I am always looking for new projects.
+          </p>
+        </div>
+        <div class="work">
+          <i class=""><GiSkills /></i>
+          <p class="work-heading">Skills</p>
+          <p class="work-text">
+            I have a lot of skills and I am very good at them. I am very good at
+            programming and I am always looking for new skills.
+          </p>
+        </div>
+        <div class="work">
+          <i class=""><LuNetwork /></i>
+          <p class="work-heading">Network</p>
+          <p class="work-text">
+            I have a lot of network skills and I am very good at them. I am very
+            good at networking and I am always looking for new network skills.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
